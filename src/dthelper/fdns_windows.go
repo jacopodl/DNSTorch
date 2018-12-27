265280c13ceedf2cd837c8eb0db38635dc85b7f4
jacopodl <jacopo.delu@gmail.com>
2018-12-27 12:17:25 +0100

    Implemented DefaultDNS for Windows, refactoring

--- src/dthelper/fdns_windows.go
+++ src/dthelper/fdns_windows.go
@@ -1,5 +1,20 @@
 package dthelper
 
+import (
+	"fmt"
+	"golang.org/x/sys/windows/registry"
+	"net"
+)
+
 func DefaultDNS() (net.IP, error) {
-	panic("not implemented yet")
+	key, err := registry.OpenKey(registry.LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters", registry.QUERY_VALUE)
+	if err != nil {
+		return nil, err
+	}
+	defer key.Close()
+
+	if v, _, err := key.GetStringValue("DhcpNameServer"); err == nil {
+		return net.ParseIP(v), nil
+	}
+	return nil, fmt.Errorf("nameserver not found")
 }
