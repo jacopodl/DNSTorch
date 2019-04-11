3bdab90adcbc90479c75ec70ff9f5b15604c9b00
jacopodl <jacopo.delu@gmail.com>
2019-04-11 14:45:26 +0200

    dSock improvements

--- src/dns/resolver/dsock.go
+++ src/dns/resolver/dsock.go
@@ -7,15 +7,13 @@ import (
 	"time"
 )
 
-// TODO: how do I get UDP/TCP packet timestamp in Go?
-// There is a way? :(
-
 type dSocket struct {
-	server  net.IP
-	port    int
-	tcp     bool
-	conn    net.Conn
-	timeout time.Duration
+	server   net.IP
+	port     int
+	tcp      bool
+	compress bool
+	conn     net.Conn
+	timeout  time.Duration
 }
 
 func newDSock(server net.IP, port int, tcp bool, timeout time.Duration) (*dSocket, error) {
@@ -23,14 +21,15 @@ func newDSock(server net.IP, port int, tcp bool, timeout time.Duration) (*dSocke
 	if err != nil {
 		return nil, err
 	}
-	return &dSocket{server, port, tcp, conn, timeout * time.Millisecond}, nil
+	return &dSocket{server, port, tcp, true, conn, timeout * time.Millisecond}, nil
 }
 
 func (d *dSocket) ask(query *Query) (*Response, error) {
 	var lookup = &Response{Query: query}
 	var dPkt = &dns.Dns{}
 	var buf []byte = nil
-	var err error = nil
+
+	dPkt.Identification = query.Id
 
 	dPkt.Authoritative = query.AA
 	dPkt.RecursionDesired = query.RD
@@ -38,67 +37,83 @@ func (d *dSocket) ask(query *Query) (*Response, error) {
 	dPkt.CheckingDisabled = query.CD
 	dPkt.AddQuestion(&query.Query)
 
-	buf = prepareBuf(dPkt, d.tcp)
+	buf = d.prepareBuf(dPkt, d.tcp)
 
 	if _, err := d.conn.Write(buf); err != nil {
 		return nil, err
 	}
 
 	if !d.tcp {
-		err = d.recvUDP(lookup)
-	} else {
-		err = d.recvTCP(lookup)
+		return d.recvUDP(lookup)
 	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	return lookup, nil
+	return d.recvTCP(lookup)
 }
 
 func (d *dSocket) close() {
-	d.conn.Close()
+	_ = d.conn.Close()
 }
 
-func (d *dSocket) recvUDP(lookup *Response) error {
-	buf := make([]byte, dns.MAXLEN)
+func (d *dSocket) prepareBuf(dPkt *dns.Dns, tcp bool) []byte {
+	buf := dPkt.ToBytes(d.compress)
+	if tcp {
+		preamble := []byte{0x00, 0x00}
+		binary.BigEndian.PutUint16(preamble, uint16(len(buf)))
+		buf = append(preamble, buf...)
+	}
+	return buf
+}
+
+func (d *dSocket) recvUDP(response *Response) (*Response, error) {
+	var length = 0
+	var buf = make([]byte, dns.MAXLEN)
+	var err error = nil
 
 	if d.timeout != 0 {
-		d.conn.SetReadDeadline(time.Now().Add(d.timeout))
-	}
-
-	if length, err := d.conn.Read(buf); err != nil {
-		return err
-	} else {
-		lookup.Msg = dns.FromBytes(buf[:length])
-		return nil
-	}
-}
-
-func (d *dSocket) recvTCP(lookup *Response) error {
-	buf := []byte{0x00, 0x00}
-	clen := 0
-	tlen := -1
-
-	for clen != tlen {
-		if length, err := d.conn.Read(buf[clen:]); err != nil {
-			return err
-		} else {
-			if tlen < 0 {
-				tlen = int(binary.BigEndian.Uint16(buf))
-				buf = make([]byte, tlen)
-				continue
-			}
-			clen += length
+		if err := d.conn.SetReadDeadline(time.Now().Add(d.timeout)); err != nil {
+			return nil, err
 		}
 	}
-	lookup.Msg = dns.FromBytes(buf[:tlen])
-	return nil
+
+	if length, err = d.conn.Read(buf); err != nil {
+		return nil, err
+	}
+
+	response.Msg = dns.FromBytes(buf[:length])
+	return response, nil
+}
+
+func (d *dSocket) recvTCP(lookup *Response) (*Response, error) {
+	var buf = []byte{0x00, 0x00}
+	var length = 0
+	var currLen = 0
+	var preamble = -1
+	var err error = nil
+
+	for currLen != preamble {
+		if length, err = d.conn.Read(buf[currLen:]); err != nil {
+			return nil, err
+		}
+		if preamble < 0 {
+			preamble = int(binary.BigEndian.Uint16(buf))
+			buf = make([]byte, preamble)
+			continue
+		}
+		currLen += length
+	}
+	lookup.Msg = dns.FromBytes(buf[:preamble])
+	return lookup, nil
+}
+
+func (d *dSocket) setCompression(enable bool) {
+	d.compress = enable
 }
 
 func (d *dSocket) switchProto() (*dSocket, error) {
-	return newDSock(d.server, d.port, !d.tcp, d.timeout)
+	ds, err := newDSock(d.server, d.port, !d.tcp, d.timeout)
+	if err == nil && !d.compress {
+		ds.setCompression(false)
+	}
+	return ds, err
 }
 
 func openConn(server net.IP, port int, tcp bool) (net.Conn, error) {
@@ -106,14 +121,4 @@ func openConn(server net.IP, port int, tcp bool) (net.Conn, error) {
 		return net.DialTCP("tcp", nil, &net.TCPAddr{IP: server, Port: port})
 	}
 	return net.DialUDP("udp", nil, &net.UDPAddr{IP: server, Port: port})
-}
-
-func prepareBuf(dPkt *dns.Dns, tcp bool) []byte {
-	buf := dPkt.ToBytes(true)
-	if tcp {
-		preamble := []byte{0x00, 0x00}
-		binary.BigEndian.PutUint16(preamble, uint16(len(buf)))
-		buf = append(preamble, buf...)
-	}
-	return buf
 }
